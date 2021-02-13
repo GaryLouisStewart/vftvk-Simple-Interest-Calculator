2bbe28181b7f8efcd1105e8c78c5c6c4c54886da
Gary Stewart <gary.stewart@example.com>
2021-02-13 21:54:01 +0000

    Completed IBM introduction to cloud development

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+
+    interest = principal * years * rate / 100;
+    abs_year = convertyears(years);
+
+    output = '<br/> If you deposit <mark>' + principal + '</mark>,<br/> at an interest rate of <mark>' + rate + '%</mark>.<br/> You will receive an amount of <mark>' + interest + '</mark>,<br/> in the year <mark>' + abs_year + '</mark><br/>'
+
+    document.getElementById("result").innerHTML = output;
 }
-        
+
+function displayrange(newvalue) {
+    document.getElementById("range-value").innerHTML=newvalue + "%";
+}
+
+function convertyears(year) {
+    d = new Date();
+    y = parseInt(d.getFullYear()) + parseInt(year);
+    return y
+}
+
+function returnDropdownValue(selectObject) {
+    var value = selectObject.value;
+    return parseInt(value)
+}
+
+function validatePrincipal() {
+    var principal = document.getElementById("principal").value;
+
+    if (principal <= 0 ){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
